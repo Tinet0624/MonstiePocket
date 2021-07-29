abf7e320736a4a5ce3998d4d6eab63a9d7df5406
Timothy Fox <timothyfox443@gmail.com>
2021-07-29 13:29:32 -0700

    created gallery form

---
+++ MonstiePocket/MainParent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MonstiePocket
+{
+    public partial class MainParent : Form
+    {
+        public MainParent()
+        {
+            InitializeComponent();
+            frmMonstieGallery monstieGallery = new frmMonstieGallery();
+            monstieGallery.MdiParent = this;
+            if (ActiveMdiChild == null)
+            {
+                monstieGallery.Show();
+            }
+        }
+    }
+}
