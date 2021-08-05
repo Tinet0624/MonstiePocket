c816a777958e29c5378d28898ab5ba476a6f5403
Alexandra Haley <72041575+Tinet0624@users.noreply.github.com>
2021-08-05 11:34:38 -0700

    Made Small Tweaks to buttons

--- MonstiePocket/FrmMonstieDisplay.Designer.cs
+++ MonstiePocket/FrmMonstieDisplay.Designer.cs
@@ -57,6 +57,7 @@ namespace MonstiePocket
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -78,7 +79,7 @@ namespace MonstiePocket
             this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.button2.Location = new System.Drawing.Point(234, 376);
+            this.button2.Location = new System.Drawing.Point(144, 376);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 37);
             this.button2.TabIndex = 75;
@@ -325,14 +326,13 @@ namespace MonstiePocket
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(361, 376);
+            this.button1.Location = new System.Drawing.Point(321, 376);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(39, 37);
+            this.button1.Size = new System.Drawing.Size(79, 37);
             this.button1.TabIndex = 57;
-            this.button1.Text = "-";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.button1.Text = "Delete";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // pictureBox3
@@ -351,11 +351,24 @@ namespace MonstiePocket
             this.pictureBox2.TabIndex = 55;
             this.pictureBox2.TabStop = false;
             // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Gray;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(11, 376);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(72, 37);
+            this.button3.TabIndex = 77;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
             // FrmMonstieDisplay
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(446, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label14);
@@ -423,5 +436,6 @@ namespace MonstiePocket
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
     }
 }
