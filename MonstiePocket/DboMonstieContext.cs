ad1cd2763dcafd5d81ceb68023af85daef093770
timothyfox443 <timothyfox443@gmail.com>
2021-08-12 15:25:46 -0700

    Added Entity Created the EDbO and linked it to the Monstie class instantiated the db and checked functionality and data input still need to modify the column properties to reflect the monstie concept but that is something that can be done later

---
+++ MonstiePocket/DboMonstieContext.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MonstiePocket
+{
+    public class DboMonstieContext : DbContext
+    {
+        // Your context has been configured to use a 'DboMonstieContext' connection string from your application's 
+        // configuration file (App.config or Web.config). By default, this connection string targets the 
+        // 'MonstiePocket.DboMonstieContext' database on your LocalDb instance. 
+        // 
+        // If you wish to target a different database and/or database provider, modify the 'DboMonstieContext' 
+        // connection string in the application configuration file.
+        public DboMonstieContext()
+            : base("name=DboMonstieContext")
+        {
+        }
+
+        // Add a DbSet for each entity type that you want to include in your model. For more information 
+        // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<Monstie> Monsties { get; set; }
+
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
